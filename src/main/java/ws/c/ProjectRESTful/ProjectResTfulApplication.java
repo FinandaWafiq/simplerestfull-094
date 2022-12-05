14a214819cb6a34c114572a27cc2aafdf71c3f39
Finanda Wafiq <finnandawafiq@gmail.com>
2022-12-05 16:05:15 +0700

    Menambahkan if pada bagian PUT

---
+++ src/main/java/ws/c/ProjectRESTful/ProjectResTfulApplication.java
@@ -0,0 +1,13 @@
+package ws.c.ProjectRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectResTfulApplication.class, args);
+	}
+
+}
